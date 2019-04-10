29bf11ab841d3f7a2431056cd6fc17d815813cb9
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-10 01:28:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  newArray = []
+  newArray.size = array.size
+  index = 0
+  for |x| in array
+    y = x * x
+    newArray[index] = y
+    index++
+  end
+    
+  return newArray
 end
