df1084df068b5c2e69dd39bfd916e837923e9b80
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-10 01:32:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
   newArray = []
-  index = 0
   array.each do |x|
     y = array[index] * array[index]
     newArray.push(y)
-    index += 1
   end
     
   return newArray
